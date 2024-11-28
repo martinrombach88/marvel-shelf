88c5cd5d8283663326bab22e50efb70c639dec7a
Martin Rombach <martinrombach88@gmail.com>
2024-11-28 21:36:57 +0000

    fixes from previous app

--- layout.tsx
+++ layout.tsx
@@ -1,22 +1,21 @@
-import type { Metadata } from "next"
-import localFont from "next/font/local"
-import "./globals.css"
+import type { Metadata } from "next";
+import "./globals.css";
 
 /* if you want to change the fonts, important them or add to globals.css */
 
 export const metadata: Metadata = {
   title: "Task List JS Testing",
   description: "Search",
-}
+};
 
 export default function RootLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode
+  children: React.ReactNode;
 }>) {
   return (
     <html lang="en">
       <body>{children}</body>
     </html>
-  )
+  );
 }
